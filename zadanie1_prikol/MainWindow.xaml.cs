018acf27573f8ccd40c38faf1bba38c357802ed4
GitRat <anal_compliment2004@mail.ru>
2024-02-22 12:43:39 +0300

    Added factorial logic

---
+++ zadanie1_prikol/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace zadanie1_prikol
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        static int res;
+
+        public static int Calculate(int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                res *= i;
+            }
+            return res;
+        }
+    }
+}
